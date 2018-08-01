1a3c8c461a674dc5bae9b73575933bc9b9aedba3
Cihan Kaptan <l5208019@std.yildiz.edu.tr>
2018-08-01 13:20:28 +0300

    Subrouting

--- src/main/kotlin/ServerVerticle.kt
+++ src/main/kotlin/ServerVerticle.kt
@@ -23,12 +23,8 @@ class ServerVerticle: CoroutineVerticle(){
 
     private fun router(): Router{
         val router = Router.router(vertx)
-        // Our router code comes here now
-        /**
-         * This will tell Vert.x to parse the request body into JSON for any request.
-         */
-        router.route().handler(BodyHandler.create())
 
+        router.mountSubRouter("/api/v1",apiRouter())
 
         router.get("/alive").asyncHandler {
             // Some response comes here
@@ -39,18 +35,29 @@ class ServerVerticle: CoroutineVerticle(){
             it.response().end(json)
         }
 
-        router.post("/api/v1/cats").asyncHandler { ctx ->
+        return router
+    }
+
+    private fun apiRouter(): Router{
+        val router = Router.router(vertx)
+        /**
+         * This will tell Vert.x to parse the request body into JSON for any request.
+         */
+        router.route().handler(BodyHandler.create())
+
+        router.post("/cats").asyncHandler { ctx ->
             // Some code of adding a cat comes here
         }
 
-        router.get("/api/v1/cats").asyncHandler { ctx ->
+        router.get("/cats").asyncHandler { ctx ->
             // Code for getting all the cats
         }
 
-        router.get("/api/v1/cats/:id").asyncHandler {ctx ->
+        router.get("/cats/:id").asyncHandler {ctx ->
             // Fethces spesific cat
         }
 
         return router
+
     }
 }
